a3797e0e241ce1305e4fed19a908d268ba72aa9e
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-20 13:04:04 +0200

    Initial Commit

---
+++ Cache/CacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheApp.swift
+//  Cache
+//
+//  Created by Skorobogatow, Christian on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
